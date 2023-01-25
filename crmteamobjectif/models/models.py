788f17d4f2efde1201cbe02980cdbc40e4d96f5a
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2023-01-25 14:14:59 +0100

    Update models.py

--- crmteamobjectif/models/models.py
+++ crmteamobjectif/models/models.py
@@ -6,10 +6,13 @@ from odoo import _, api, fields, models
 class   Crmteamoinhertit(models.Model):
     _inherit = "crm.team"
 
-    crm_team_comer = fields.Monetary("Marge")
-    crm_team_chif = fields.Monetary("Chiffre d'affaire")
-    crm_team_N_client = fields.Integer("Nombre de clients")
-    crm_team_N_contrat= fields.Integer("Nombre de contrats")
+    crm_team_comer = fields.Monetary("MARGE COMMERCIALE")
+    crm_team_chif = fields.Monetary("CHIFFRE D'AFFAIRE")
+    crm_team_reel = fields.Monetary("MARGE REELLE")
+    crm_team_livraison = fields.Monetary("FRAIS DE LIVRAISON")
+    crm_team_N_client = fields.Integer("NB NEW")
+    crm_team_N_contrat= fields.Integer("NB CONTRAT")
+    crm_team_N_materiel= fields.Integer("NB MATERIELS")
     
         
     @api.onchange("member_ids")

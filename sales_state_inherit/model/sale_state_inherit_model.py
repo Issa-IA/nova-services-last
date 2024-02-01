44793e3ae6181b0192545138f42dbebb360fd67e
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2024-02-01 14:07:45 +0100

    Update sale_state_inherit_model.py

--- sales_state_inherit/model/sale_state_inherit_model.py
+++ sales_state_inherit/model/sale_state_inherit_model.py
@@ -6,21 +6,21 @@ class SaleStateHerit(models.Model):
 
     state = fields.Selection(selection_add=[('invoiced', 'Facturé')])
 
-    @api.onchange('invoice_count')
-    def update_state(self):
-        for rec in self:
-            if rec.state == 'done':
-                if rec.invoice_count > 0:
-                    rec.state = 'invoiced'
+    # @api.onchange('invoice_count')
+    # def update_state(self):
+    #     for rec in self:
+    #         if rec.state == 'done':
+    #             if rec.invoice_count > 0:
+    #                 rec.state = 'invoiced'
                 
 
-    # def write(self, vals):        
-    #     for rec in self:  
-    #         if rec.invoice_count > 0 :
-    #             if rec.state == 'done':
-    #                 vals.update({'state':'invoiced'})   
-    #         else:
-    #             if rec.state == 'invoiced':
-    #                 vals.update({'state':'done'})
-    #     res=super(SaleStateHerit, self).write(vals)
-    #     return res
+    def write(self, vals):        
+        for rec in self:  
+            if rec.state == 'done':
+                if rec.invoice_count > 0 :                
+                        vals.update({'state':'invoiced'})   
+            # else:
+            #     if rec.state == 'invoiced':
+            #         vals.update({'state':'done'})
+        res=super(SaleStateHerit, self).write(vals)
+        return res

6d2198f2bf84794e803174771704f47347c371f8
Chayma-ch23 <58692102+Chayma-ch23@users.noreply.github.com>
2024-02-29 11:06:10 +0100

    Update inherit_list_sale.py

--- Inherit_list_sale/model/inherit_list_sale.py
+++ Inherit_list_sale/model/inherit_list_sale.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class StockHerit(models.Model):
     _inherit = 'sale.order'
 
-    etiquette_maintenance = fields.Selection([('maintenance', 'Maintenance'),('import CVI', 'import'), ('autres', 'Autres'),('cvi','CVI')], compute='_maintenance_etiquette', inverse='_inverse_etiquette_maintenance', 
+    etiquette_maintenance = fields.Selection([('maintenance', 'Maintenance'),('import CVI', 'CVI import'), ('autres', 'Autres'),('cvi','CVI')], compute='_maintenance_etiquette', inverse='_inverse_etiquette_maintenance', 
 store=True)
     sale_new_contrat = fields.Integer(string="CONTRAT",default=0)
 

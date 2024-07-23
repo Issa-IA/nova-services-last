f9cb1c81f1d046f3a1b0eec07e15f5b36a6a2df3
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2024-07-23 15:12:18 +0100

    Update models.py

--- venteboncommande/models/models.py
+++ venteboncommande/models/models.py
@@ -18,7 +18,7 @@ class SaleMoveHeritbondecommande(models.Model):
                 if sale_bon_commande.sale_date_Facture:
                     if sale_bon_commande.sale_periode:
                         if sale_bon_commande.sale_periode == 1:
-                            if sale_bon_commande.sale_date_Facture + relativedelta(months=1) <= date.today()+relativedelta(days=1) and sale_bon_commande.sale_date_Facture <=sale_bon_commande.sale_date_de_fin_contrat:
+                            if sale_bon_commande.sale_date_Facture + relativedelta(months=1)-relativedelta(days=1) <= date.today() and sale_bon_commande.sale_date_Facture <=sale_bon_commande.sale_date_de_fin_contrat:
                                 if sale_bon_commande.devis_a_cree_commande == True:
                                     sale_vals = {
                                         'company_id': sale_bon_commande.company_id.id,
@@ -139,7 +139,7 @@ class SaleMoveHeritbondecommande(models.Model):
                                             self.env['sale.order.line'].sudo().create(res)
                                         
                         if sale_bon_commande.sale_periode == 3:
-                            if sale_bon_commande.sale_date_Facture + relativedelta(months=3) <= date.today()+relativedelta(days=1) and sale_bon_commande.sale_date_Facture <=sale_bon_commande.sale_date_de_fin_contrat:
+                            if sale_bon_commande.sale_date_Facture + relativedelta(months=3)-relativedelta(days=1) <= date.today() and sale_bon_commande.sale_date_Facture <=sale_bon_commande.sale_date_de_fin_contrat:
                                 if sale_bon_commande.devis_a_cree_commande == True:
                                     sale_vals = {
                                         'company_id': sale_bon_commande.company_id.id,

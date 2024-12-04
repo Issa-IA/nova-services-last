21381927121aafaa1f3005b1695684127f24a0cb
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2024-12-04 09:45:44 +0100

    Add files via upload

--- backend_theme/__manifest__.py
+++ backend_theme/__manifest__.py
@@ -2,35 +2,36 @@
 
 {
     "name": "Theme",
-    "description": """ backend theme #343434 for Odoo""",
-    "summary": "backend theme #343434 for Odoo",
-    "category": "Themes/Backend",
-    "version": "15.0.1.0.3",
+    "description": """ backend theme for Odoo""",
+    "summary": "backend theme  for Odoo",
+    "category": "",
+    "version": "0.0.1",
     'author': 'shayma',
     'company': 'shayma',
     'maintainer': 'shayma',
     'website': "www.company.com",
-    "depends": ['base', 'web', 'mail'],
+    "depends": ['base', 'mail'],
     "data": [
-        'views/layout.xml',
-    ],
-    'assets': {
-        'web.assets_frontend': [
-        ],
-        'web.assets_backend': [
-            'backend_theme/static/src/scss/theme_accent.scss',
-            'backend_theme/static/src/scss/theme.scss',
-
-        ],
-        'web.assets_qweb': [
-        ],
-    },
-    'images': [
-    ],
-    'license': 'LGPL-3',
-    'pre_init_hook': 'test_pre_init_hook',
-    'post_init_hook': 'test_post_init_hook',
-    'installable': True,
-    'application': False,
-    'auto_install': False,
+        #'views/layout.xml',
+    ]
+    # ,
+    # 'assets': {
+    #     'web.assets_frontend': [
+    #     ],
+    #     'web.assets_backend': [
+    #         'backend_theme/static/src/scss/theme_accent.scss',
+    #         'backend_theme/static/src/scss/theme.scss',
+    #
+    #     ],
+    #     'web.assets_qweb': [
+    #     ],
+    # },
+    # 'images': [
+    # ],
+    # 'license': 'LGPL-3',
+    # 'pre_init_hook': 'test_pre_init_hook',
+    # 'post_init_hook': 'test_post_init_hook',
+    # 'installable': True,
+    # 'application': False,
+    # 'auto_install': False,
 }

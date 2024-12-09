acab57a7a458fbfae06fb7f5bf147800cdf0dac1
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2024-12-09 10:56:17 +0100

    Add files via upload

--- fleetADD1/__manifest__.py
+++ fleetADD1/__manifest__.py
@@ -18,7 +18,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','fleet'],
+    'depends': ['base','fleet', 'product', 'sale'],
 
     # always loaded
     'data': [

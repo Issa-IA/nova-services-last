5c38716fc9448e1a5cf8403c4f93f3a4a0a825a8
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2024-12-09 12:21:57 +0100

    Add files via upload

--- emailsendauto/__manifest__.py
+++ emailsendauto/__manifest__.py
@@ -16,7 +16,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale'],
+    'depends': ['base','product', 'sale','contacts'],
 
     # always loaded
     'data': [

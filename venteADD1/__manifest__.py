2342b0e12eac7282e65c29d5900dee70a6aed0f0
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2024-12-09 11:34:47 +0100

    Add files via upload

--- venteADD1/__manifest__.py
+++ venteADD1/__manifest__.py
@@ -14,7 +14,7 @@
     'version': '0.1',
 
     # any module necessary for this one to work correctly
-    'depends': ['base','sale','contacts','product'],
+    'depends': ['base','sale','contacts','product', 'fleet'],
 
     # always loaded
     'data': [

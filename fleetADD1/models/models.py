19d374fb170171db6c4c770aaa593ac555e19375
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2024-12-13 11:18:58 +0100

    Add files via upload

--- fleetADD1/models/models.py
+++ fleetADD1/models/models.py
@@ -310,6 +310,20 @@ class FleetContINHERIT(models.Model):
    ############
     #  Vehicle #
     ############
+    @api.depends('fleet_serie','fleet_Modele', 'fleet_serie')
+    def _compute_display_name(self):
+        for model in self:
+            if model.fleet_marque:
+                if model.fleet_Modele:
+                    if model.fleet_serie:
+                        name = model.fleet_marque.name + '/' + model.fleet_Modele.name + '(' + model.fleet_serie + ')'
+                    else:
+                        name = model.fleet_marque.name + '/' + model.fleet_Modele.name + '(' + "Pas de N° serie" + ')'
+                else:
+                    name = model.fleet_marque.name
+            else:
+                name = "Pas de matériel"
+            model.display_name = name
     
 
 

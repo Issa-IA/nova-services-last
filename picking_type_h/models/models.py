986f10552ddff6f857bd66ca7ef71ff97bcb8f25
Issa-IA <80253673+Issa-IA@users.noreply.github.com>
2025-05-19 12:40:09 +0100

    Update models.py

--- picking_type_h/models/models.py
+++ picking_type_h/models/models.py
@@ -54,7 +54,7 @@ class Stocktypeinherit(models.Model):
     def _compute_picking_count_done(self):
         for rec in self:
             fact_count = self.env['stock.picking'].search_count(
-                [('id_type_fait', '=',1),('picking_type_id', '=',8), ('return_count','=', 0), ('state','=', 'done')])
+                [('id_type_fait', '=',1),('picking_type_id', '=',8), ('return_ids','=', False), ('state','=', 'done')])
             rec.count_picking_done = fact_count
             print("rec.count_picking_done",rec.count_picking_done)
 
@@ -64,7 +64,7 @@ class Stocktypeinherit(models.Model):
             'name': 'Prêts en cours',
             'res_model': 'stock.picking',
             'view_type': 'form',
-            'domain': [ ('id_type_fait', '=',1),('picking_type_id', '=',8), ('return_count','=', 0), ('state','=', 'done')],
+            'domain': [ ('id_type_fait', '=',1),('picking_type_id', '=',8), ('return_ids','=', False), ('state','=', 'done')],
             'view_mode': 'list,form',
 
         }
